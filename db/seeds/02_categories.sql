4d1251c99c3d8dedc0a14015f0a2d0972cfe0757
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-08-16 12:39:37 -0400

    Add initial seed data for categories table

---
+++ db/seeds/02_categories.sql
@@ -0,0 +1,21 @@
+-- category table seeds
+INSERT INTO categories (title)
+VALUES
+ ('JavaScript'),
+ ('Tools_&_Productivity'),
+ ('React'),
+ ('Design'),
+ ('HTML'),
+ ('CSS'),
+ ('Quotes'),
+ ('Databases'),
+ ('Networking'),
+ ('Python'),
+ ('Mental_health'),
+ ('Ruby'),
+ ('Git'),
+ ('Tutorials'),
+ ('Study_ambience'),
+ ('Technology'),
+ ('Events');
+ 
