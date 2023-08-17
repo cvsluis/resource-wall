9f51e65b738c0019c29af9b3157b46ba962aed34
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-08-17 10:48:45 +0100

    feat: declare pin db query functions

---
+++ db/queries/pins.js
@@ -0,0 +1,38 @@
+const db = require('../connection');
+
+const getUsers = () => {
+  return db.query('SELECT * FROM users;')
+    .then(data => {
+      return data.rows;
+    });
+};
+
+// model after LightBnB getAllProperties
+// check for categories, ratings
+// potential second parameter 
+const getAllPins = (options) => {
+
+};
+
+// takes in pin id
+// returns all information pertaining to that pin
+// also comments, likes and ratings
+const getOnePin = (pinId) => {
+
+};
+
+// model this function after LightBnB addProperty
+// takes in an object containing all of the pin details
+// returns result.rows
+const addOnePin = (pin) => {
+
+};
+
+// takes in pin id
+// returns all information pertaining to that pin
+// also comments, likes and ratings
+const editOnePin = (pinId) => {
+
+};
+
+module.exports = { getAllPins, getOnePin, addOnePin, editOnePin };
