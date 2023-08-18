d9aa38a2968269018f0c326c23d940801580e2f4
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-08-18 19:11:05 +0100

    feat: add interation functions

---
+++ db/queries/interactions.js
@@ -0,0 +1,37 @@
+const db = require('../connection');
+
+// example function for reference
+const getUsers = () => {
+  return db.query('SELECT * FROM users;')
+    .then(data => {
+      return data.rows;
+    });
+};
+
+
+// takes in an object containing all of the comment details
+// returns result.rows
+const addComment = (comment) => {
+
+};
+
+// takes in an object containing all of the rating details
+// returns result.rows
+const addRating = (rating) => {
+
+};
+
+// takes in an object containing all of the rating details
+// returns result.rows
+const addLike = (like) => {
+
+};
+
+// takes in an object containing all of the rating details
+// returns result.rows
+const removeLike = (like) => {
+
+};
+
+
+module.exports = { getPinComments, addComment, getPinRating, addRating, addLike, removeLike };
