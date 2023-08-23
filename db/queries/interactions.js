6141bc996d73007b302f4775992bda4da4efab3d
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-08-22 20:10:53 -0400

    Add query for adding comments

--- db/queries/interactions.js
+++ db/queries/interactions.js
@@ -15,18 +15,30 @@ const getAllCategories = () => {
 
 const getComments = (pinId) => {
   const queryString = `
-  SELECT *, users.name as name
-  FROM comments
-  JOIN users on users.id = comments.owner_id
-  WHERE pin_id = $1`;
+    SELECT *, users.name as name
+    FROM comments
+    JOIN users on users.id = comments.owner_id
+    WHERE pin_id = $1;
+  `;
 
   return db.query(queryString, [pinId]).then(data => data.rows);
 };
 
 // takes in an object containing all of the comment details
 // returns result.rows
-const addComment = (owner_id, comment) => {
+const addComment = (obj) => {
+  const queryParams = [obj.pin_id, obj.owner_id, obj.comment];
 
+  const queryString = `
+    INSERT INTO comments (pin_id, owner_id, description)
+    VALUES ($1, $2, $3);
+  `;
+
+  return db
+    .query(queryString, queryParams)
+    .then((result) => {
+      return result.rows;
+    });
 };
 
 // takes in an object containing all of the rating details
