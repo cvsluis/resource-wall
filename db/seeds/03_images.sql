e8e6908b13716093cdcf611ab5d0c294a99a8a36
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-08-24 18:19:23 -0400

    Add image URLs from GitHub repo

--- db/seeds/03_images.sql
+++ db/seeds/03_images.sql
@@ -1,22 +1,22 @@
 INSERT INTO images (url, alt, category_id)
 VALUES
   ('https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/640px-JavaScript-logo.png', 'JavaScript resource', 1),
-  ('https://mixkit.imgix.net/art/preview/mixkit-girl-sitting-at-her-desk-at-work-with-laptop-open-542-original-large.png?q=80&auto=format%2Ccompress', 'Tools & productivity resource', 2),
+  ('https://github.com/cvsluis/resource-wall/blob/master/public/assets/tools-productivity.png?raw=true', 'Tools & productivity resource', 2),
   ('https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png?20220125121207', 'React resource', 3),
-  ('https://mixkit.imgix.net/art/preview/mixkit-girl-sitting-at-her-desk-at-work-with-laptop-open-542-original-large.png?q=80&auto=format%2Ccompress', 'Design resource', 4),
+  ('https://github.com/cvsluis/resource-wall/blob/master/public/assets/design.png?raw=true', 'Design resource', 4),
   ('https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png?20170517184425', 'HTML5 logo', 5),
   ('https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/363px-CSS3_logo_and_wordmark.svg.png?20160530175649', 'CSS3 resource', 6),
-  ('https://mixkit.imgix.net/art/preview/mixkit-girl-sitting-at-her-desk-at-work-with-laptop-open-542-original-large.png?q=80&auto=format%2Ccompress', 'Quotes', 7),
+  ('https://github.com/cvsluis/resource-wall/blob/master/public/assets/quotes.png?raw=true', 'Quotes', 7),
   ('https://icones.pro/wp-content/uploads/2021/05/icone-base-donnees-jaune.png', 'Database resource', 8),
-  ('https://mixkit.imgix.net/art/preview/mixkit-girl-sitting-at-her-desk-at-work-with-laptop-open-542-original-large.png?q=80&auto=format%2Ccompress', 'Networking resource', 9),
+  ('https://github.com/cvsluis/resource-wall/blob/master/public/assets/networking.png?raw=true', 'Networking resource', 9),
   ('https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png', 'Python resource', 10),
-  ('https://mixkit.imgix.net/art/preview/mixkit-girl-sitting-at-her-desk-at-work-with-laptop-open-542-original-large.png?q=80&auto=format%2Ccompress', 'Mental health resource', 11),
+  ('https://github.com/cvsluis/resource-wall/blob/master/public/assets/mental-health.png?raw=true', 'Mental health resource', 11),
   ('https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Ruby_logo.svg/1024px-Ruby_logo.svg.png', 'Ruby resource', 12),
   ('https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png', 'Git resource', 13),
-  ('https://mixkit.imgix.net/art/preview/mixkit-girl-sitting-at-her-desk-at-work-with-laptop-open-542-original-large.png?q=80&auto=format%2Ccompress', 'Tutorials', 14),
-  ('https://mixkit.imgix.net/art/preview/mixkit-girl-sitting-at-her-desk-at-work-with-laptop-open-542-original-large.png?q=80&auto=format%2Ccompress', 'Study ambience resource', 15),
-  ('https://mixkit.imgix.net/art/preview/mixkit-girl-sitting-at-her-desk-at-work-with-laptop-open-542-original-large.png?q=80&auto=format%2Ccompress', 'Technology', 16),
-  ('https://mixkit.imgix.net/art/preview/mixkit-girl-sitting-at-her-desk-at-work-with-laptop-open-542-original-large.png?q=80&auto=format%2Ccompress', 'Events', 17)
+  ('https://github.com/cvsluis/resource-wall/blob/master/public/assets/tutorials.png?raw=true', 'Tutorials', 14),
+  ('https://github.com/cvsluis/resource-wall/blob/master/public/assets/study-ambience.png?raw=true', 'Study ambience resource', 15),
+  ('https://github.com/cvsluis/resource-wall/blob/master/public/assets/technology.png?raw=true', 'Technology', 16),
+  ('https://github.com/cvsluis/resource-wall/blob/master/public/assets/events.png?raw=true', 'Events', 17)
 ;
 
 -- About image alt text and accessibility:
