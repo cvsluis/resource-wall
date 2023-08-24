7f7d51f9813e7cb7e7dc1b6adb1d5a398027ba47
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-08-24 17:21:17 -0400

    Add temporary image (will need to access assets when merged to main branch)

--- db/seeds/07_images.sql
+++ db/seeds/07_images.sql
@@ -1,22 +1,22 @@
 INSERT INTO images (url, alt, category_id)
 VALUES
   ('https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/640px-JavaScript-logo.png', 'JavaScript resource', 1),
-  (url, 'Tools & productivity resource', 2),
+  ('https://mixkit.imgix.net/art/preview/mixkit-girl-sitting-at-her-desk-at-work-with-laptop-open-542-original-large.png?q=80&auto=format%2Ccompress', 'Tools & productivity resource', 2),
   ('https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png?20220125121207', 'React resource', 3),
-  (url, 'Design resource', 4),
+  ('https://mixkit.imgix.net/art/preview/mixkit-girl-sitting-at-her-desk-at-work-with-laptop-open-542-original-large.png?q=80&auto=format%2Ccompress', 'Design resource', 4),
   ('https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png?20170517184425', 'HTML5 logo', 5),
   ('https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/363px-CSS3_logo_and_wordmark.svg.png?20160530175649', 'CSS3 resource', 6),
-  (url, 'Quotes', 7),
+  ('https://mixkit.imgix.net/art/preview/mixkit-girl-sitting-at-her-desk-at-work-with-laptop-open-542-original-large.png?q=80&auto=format%2Ccompress', 'Quotes', 7),
   ('https://icones.pro/wp-content/uploads/2021/05/icone-base-donnees-jaune.png', 'Database resource', 8),
-  (url, 'Networking resource', 9),
+  ('https://mixkit.imgix.net/art/preview/mixkit-girl-sitting-at-her-desk-at-work-with-laptop-open-542-original-large.png?q=80&auto=format%2Ccompress', 'Networking resource', 9),
   ('https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png', 'Python resource', 10),
-  (url, 'Mental health resource', 11),
+  ('https://mixkit.imgix.net/art/preview/mixkit-girl-sitting-at-her-desk-at-work-with-laptop-open-542-original-large.png?q=80&auto=format%2Ccompress', 'Mental health resource', 11),
   ('https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Ruby_logo.svg/1024px-Ruby_logo.svg.png', 'Ruby resource', 12),
   ('https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png', 'Git resource', 13),
-  (url, 'Tutorials', 14),
-  (url, 'Study ambience resource', 15),
-  (url, 'Technology', 16),
-  (url, 'Events', 17)
+  ('https://mixkit.imgix.net/art/preview/mixkit-girl-sitting-at-her-desk-at-work-with-laptop-open-542-original-large.png?q=80&auto=format%2Ccompress', 'Tutorials', 14),
+  ('https://mixkit.imgix.net/art/preview/mixkit-girl-sitting-at-her-desk-at-work-with-laptop-open-542-original-large.png?q=80&auto=format%2Ccompress', 'Study ambience resource', 15),
+  ('https://mixkit.imgix.net/art/preview/mixkit-girl-sitting-at-her-desk-at-work-with-laptop-open-542-original-large.png?q=80&auto=format%2Ccompress', 'Technology', 16),
+  ('https://mixkit.imgix.net/art/preview/mixkit-girl-sitting-at-her-desk-at-work-with-laptop-open-542-original-large.png?q=80&auto=format%2Ccompress', 'Events', 17)
 ;
 
 -- About image alt text and accessibility:
